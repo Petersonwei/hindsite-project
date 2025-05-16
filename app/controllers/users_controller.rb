25b71d72f5f04ce24a5e92ada8ea3a8e5330011b
petersonwei <weipetersonwei@gmail.com>
2025-05-16 21:43:14 +1000

    Fix profile update by removing current_password from user update params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,24 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update(user_params)
+    # Check if attempting to change password
+    if password_change_attempted?
+      # Verify current password
+      unless @user.authenticate(params[:user][:current_password])
+        @user.errors.add(:current_password, "is incorrect")
+        return render :edit, status: :unprocessable_entity
+      end
+    end
+    
+    # Set user_update_params without current_password
+    user_update_params = params.require(:user).permit(:name, :email, :password, :password_confirmation, :organisation_id)
+    
+    # If password is blank, don't update it
+    if user_update_params[:password].blank?
+      user_update_params = user_update_params.except(:password, :password_confirmation)
+    end
+    
+    if @user.update(user_update_params)
       redirect_to @user, notice: 'User was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
@@ -48,5 +65,9 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :organisation_id)
+  end
+  
+  def password_change_attempted?
+    params[:user] && params[:user][:password].present?
   end
 end
